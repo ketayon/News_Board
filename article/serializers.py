d1ead2df12c922bb86cbb1c02068bfbadd522263
ketayon <ketayon@gmail.com>
2020-06-26 12:06:20 +0300

    Deploy

--- article/serializers.py
+++ article/serializers.py
@@ -11,4 +11,10 @@ class MessageSerializer(serializers.Serializer):
     def create(self, validated_data):
         return Message.objects.create(**validated_data)
 
-    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.link = validated_data.get('link', instance.link)
+        instance.amount_of_upvotes = validated_data.get('amount_of_upvotes', instance.amount_of_upvotes)
+        instance.author_id = validated_data.get('author_id', instance.author_id)
+        instance.save()
+        return instance
